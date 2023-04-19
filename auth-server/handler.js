ebf508110ff8f1722be918700407fc22bdf42bb1
SwethaaSundar <swethaasundar@gmail.com>
2023-04-19 19:26:00 +0200

    adding header

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -103,6 +103,10 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
         body: JSON.stringify(err),
       };
     });
