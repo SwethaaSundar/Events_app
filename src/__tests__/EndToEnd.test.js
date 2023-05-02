0b2b4061b95f9a99ae6d083b3ed5767c672b2bb1
SwethaaSundar <swethaasundar@gmail.com>
2023-05-02 15:13:55 +0200

    Error fix

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,12 +4,26 @@ import puppeteer from "puppeteer";
 describe("show/hide an event details", () => {
   let browser;
   let page;
-  jest.setTimeout(40000);
+  // jest.setTimeout(40000);
 
   beforeAll(async () => {
+    // jest.setTimeout(40000);
+    // browser = await puppeteer.launch();
+    // //   browser = await puppeteer.launch({
+    // //     headless: false,
+    // //     // slowMo: 250, // slow actions by 250ms
+    // //     ignoreDefaultArgs: ["--disable-extensions"], // ignores default settings
+    // //   });
+
+    // page = await browser.newPage();
+    // await page.goto("http://localhost:3000/");
+    // await page.waitForSelector(".event");
+
+    jest.setTimeout(40000);
+
     browser = await puppeteer.launch({
       headless: false,
-      // slowMo: 250, // slow actions by 250ms
+      slowMo: 250, // slow actions by 250ms
       ignoreDefaultArgs: ["--disable-extensions"], // ignores default settings
     });
 
@@ -28,15 +42,15 @@ describe("show/hide an event details", () => {
   });
 
   test("User can expand an event to seeits details", async () => {
-    await page.click(".event .details-button");
+    await page.click(".event .details-btn");
 
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeDefined();
   });
 
   test("User can collapse an event to hide its details", async () => {
-    await page.click(".event .details-button");
-    const eventDetails = await page.$(".event .details");
+    await page.click(".event .details-btn");
+    const eventDetails = await page.$(".event .description");
     expect(eventDetails).toBeNull();
   });
 });
